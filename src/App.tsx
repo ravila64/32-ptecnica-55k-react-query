7ad879f4360d982b7e4af94d030df7b41f177376
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2024-11-10 16:32:09 -0500

    fix @tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo, useState } from 'react'
+//import { useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
 import { UsersList } from './components/UsersList.tsx'
 import { SortBy, type User } from './types/types.d'
 
 const fetchUsers = async (page: number) => {
   return await fetch('https://randomuser.me/api?results=10&seed=midudev&page=${page}')
-      // v.2.0
+      // v.2.
       .then(async res=>{
-        console.log("Estados del res.ok ", res.ok, " res.status ", res.status, " res.statusText ", res.statusText);
-        if(!res.ok) throw new Error('Error en la petición');
-        return await res.json();
+        console.log("Page now ",page)
+        console.log("status of res.ok ", res.ok, " res.status ", res.status, " res.statusText ", res.statusText)
+        if(!res.ok) throw new Error('Error en la petición')
+        return await res.json()
       })
-      .then(res=>res.results);
+      .then(res=>res.results)
 }
 
 function App() {
+  const {isLoading, isError,  data: users=[] } = useQuery<User[]>(
+    ['users'],
+    async () => await fetchUsers(1)
+  )
+
   // array de users
-  const [users, setUsers] = useState<User[]>([])
+  //const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   //const [sortByCountry, setSortByCountry] = useState(false)
   const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
   const [filterCountry, setFilterCountry] = useState<string | null>(null)
   
-  const [loading, setLoading] = useState(false);  //ver 2.0
-  const [error, setError] = useState(false); //v.2.0
+  //const [loading, setLoading] = useState(false);  //ver 2.0
+  //const [error, setError] = useState(false); //v.2.0
   const [currentPage, setCurrentPage] = useState(1); //v.2.0
 
-  const originalUsers = useRef<User[]>([])
+  //const originalUsers = useRef<User[]>([])
 
   //const [originalUsers, setOroginalUsers] = useState<User[]>([]);
   // esta linea anterior no se debe hacer, crear otro useState
@@ -46,43 +54,43 @@ function App() {
     // setSortByCountry(prevState => !prevState)
   }
   const handleReset = () => {
-    setUsers(originalUsers.current)
+    //setUsers(originalUsers.current)
   }
 
   const handleDelete = (email: string) => {
-    // filteredUsers = users.filter((user, userIndex)
-    // userIndex !== index
-    const filteredUsers = users.filter((user) => user.email !== email)
-    setUsers(filteredUsers)
+    // // filteredUsers = users.filter((user, userIndex)
+    // // userIndex !== index
+    // const filteredUsers = users.filter((user) => user.email !== email)
+    // setUsers(filteredUsers)
   }
 
   const handleChangeSort = (sort: SortBy) =>{
     setSorting(sort)
   }
 
-  useEffect(() => {
-    setLoading(true); //v.2.0
-    setError(false);
-    // v.2.0
-    fetchUsers(currentPage)
-       .then (users=>{
-          setUsers(prevUsers =>{
-            const newUsers= prevUsers.concat(users)
-            originalUsers.current = newUsers
-            return newUsers
-          })
-    })
-    .catch(err => {
-        // v.2.0
-        setError(err);
-        console.log(err)
-      })
+  // useEffect(() => {
+  //   setLoading(true); //v.2.0
+  //   setError(false);
+  //   // v.2.0
+  //   fetchUsers(currentPage)
+  //      .then (users=>{
+  //         setUsers(prevUsers =>{
+  //           const newUsers= prevUsers.concat(users)
+  //           originalUsers.current = newUsers
+  //           return newUsers
+  //         })
+  //   })
+  //   .catch(err => {
+  //       // v.2.0
+  //       setError(err);
+  //       console.log(err)
+  //     })
 
-      //v.2.0
-      .finally(()=>{
-        setLoading(false);
-      })
-  }, [currentPage])  // cada vez que cambie el valor de la var currentPage
+  //     //v.2.0
+  //     .finally(()=>{
+  //       setLoading(false);
+  //     })
+  // }, [currentPage])  // cada vez que cambie el valor de la var currentPage
 
   // tambien puede ser: const filteredUsers=filterCountry !== nul && filterCountry.length > 0
   const filteredUsers = useMemo(() => {
@@ -134,11 +142,12 @@ function App() {
         }} />
 
       </header>
+      {/* v.2.0  */}
       <main>
-        {/* v.2.0 */}
         { users.length>0 && 
           <UsersList changeSorting={handleChangeSort} deleteUser={handleDelete} showColors={showColors}
-          users={sortedUsers} /> }
+          users={sortedUsers} /> 
+        }
 
         {loading && <strong>Cargando ...</strong>}
         {!loading && error && <p>Ha habido un error ...</p>}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+//fix v.2.0
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<React.StrictMode>
+//<React.StrictMode>  se utiliza Para  ENVOLVER
+<QueryClientProvider client = {queryClient}>
   <App />
-</React.StrictMode>
+</QueryClientProvider>
+//</React.StrictMode>
 )
